5f7dd7561d2980004d448db19c4a61d956471330
Tamás Danyluk <tdanyluk@google.com>
2022-10-11 13:21:03 +0200

    Add Powershell script to regenerate test outputs

---
+++ tests/regenerate_test_outputs.ps1
@@ -0,0 +1,3 @@
+python .\mac2winKeyboard.py -o tests .\tests\dummy.keylayout
+python .\mac2winKeyboard.py -o tests .\tests\sgcap.keylayout
+python .\mac2winKeyboard.py -o tests .\tests\us_test.keylayout
